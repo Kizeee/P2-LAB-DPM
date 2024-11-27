d82b7baf99cf39eba4bec5fb248ce48aafe64b41
Kizeee <m4ulanarizky261@gmail.com>
2024-11-27 21:52:53 +0700

    upload

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Teks di tengah layar */}
+      <Text style={styles.text}>2 kotak dengan warna yang berbeda</Text>
+
+      {/* Wrapper untuk kotak */}
+      <View style={styles.boxContainer}>
+        {/* Kotak 1 */}
+        <View style={[styles.box, { backgroundColor: "yellow" }]}></View>
+        {/* Kotak 2 */}
+        <View style={[styles.box, { backgroundColor: "purple" }]}></View>
+      </View>
     </View>
   );
 }
@@ -13,8 +21,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#d3d3d3",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    color: "blue",
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  boxContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    paddingHorizontal: 50,
+  },
+  box: {
+    width: 100,
+    height: 100,
   },
 });
